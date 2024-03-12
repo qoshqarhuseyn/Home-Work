d215e558a050a616209c07b31c7dcddf5d29a815
qoshqarhuseyn <qoshqarhuseyn1@gmail.com>
2024-03-12 16:31:23 +0400

    tasks 1-14

---
+++ Qoshqar Task 1.py
@@ -0,0 +1,195 @@
+# Task 1 - Pərakəndə
+
+# product_details = {}
+
+# for _ in range(6):
+#     product_name = input("Enter the product name: ")
+#     product_price = float(input("Enter the product price: ₼"))
+    
+#     product_details[product_name] = product_price
+
+# print("\nProduct Details:")
+# for name, price in product_details.items():
+#     print(f"{name}: ₼{price:.2f}")
+
+
+
+
+# Task 2 - Maliyyə
+
+# total_value = 0
+
+# for i in range(3):
+#     share_count = int(input(f"Enter the number of shares for the {i + 1}. stock: "))
+#     share_price = float(input(f"Enter the share price for the {i + 1}. stock: "))
+    
+#     stock_value = share_count * share_price
+#     total_value += stock_value
+
+# print(f"\nThe total value of the stock portfolio is: ${total_value:.2f}")
+
+
+
+
+# Task 3 - Səhiyyə
+
+# details = {}
+
+# for _ in range(5):
+#     name = input("Enter the  name: ")
+#     age = float(input("Enter  Age:?"))
+    
+#     details[name] = age
+
+# print("\n Details:")
+# for name, age in details.items():
+#     print(f"{name}: {age:.2f}")
+
+
+
+# Task 4 - Təhsil
+
+# details = {}
+
+# for _ in range(8):
+#     name = input("Enter Student name: ")
+#     age = float(input("Enter  Age:?"))
+    
+#     details[name] = age
+
+# print("\n Details:")
+# for name, age in details.items():
+#     print(f"{name}: {age:.2f}")
+
+
+
+
+# Task 5 - Restoran
+    
+# details = {}
+# for _ in range(2):
+
+#    name = input("Enter Food name: ")
+#    price = float(input("Enter in price: "))
+
+
+#    details[name] = price
+
+# print("Details")
+# for name, price in details.items():
+#     print(f"{name}: {price:.2f}")
+
+
+
+# Task 6 - Texnologiya:
+
+# for _ in range(4):
+#     developer_name = input("Enter the progranmist name: ")
+#     age = int(input("Enter the programist age: "))
+#     programming_language = input("Enter the programming language used: ")
+
+#     formatted_output = f"Developer: {developer_name}, Age: {age}, Programming Language: {programming_language}"
+#     print(formatted_output)
+
+
+
+# Task 7 - Nəqliyyat:
+
+
+
+
+# distance = float(input("Car Distance enter (kilometr): "))
+# time = float(input("Car clock enter (clock): "))
+
+# average_speed = distance / time
+
+# print(f"\n Avarage: {average_speed``:.2f} km/clock")
+
+
+
+
+
+# Task 8 - Əyləncə:
+
+
+# for _ in range(4):
+#     movie_title = input("Enter the movie title: ")
+#     release_date = input("Enter the release date: ")
+
+#     formatted_output = f"Movie Title: {movie_title}, Release Date: {release_date}"
+#     print(formatted_output)
+
+
+
+
+
+
+
+# Task 9 - Daşınmaz Əmlak:
+
+# for _ in range(2):
+#     square_footage = float(input("Enter footage  house: "))
+#     price = float(input("Enter the price  house: "))
+
+#     formatted_output = f"Square Footage: {square_footage} sq. ft, Price: ${price}"
+#     print(formatted_output)
+
+
+
+# Task 10 - Elektron ticarət:
+
+
+
+# for _ in range(2):
+#     product_name = input("Enter the product name: ")
+#     price = float(input("Enter the price of the product: "))
+
+#     formatted_output = f"Product Name: {product_name}, Price: ₼{price}"
+#     print(formatted_output)
+
+
+
+# Task 11 - Kənd Təsərrüfatı:
+
+# product_name = input("Enter the vegetable name: ")
+# price = float(input("Enter the price: "))
+
+# formatted_output = f"Product Name: {product_name}, Price: ₼{price}"
+# print(formatted_output)
+
+
+
+
+
+# Task 12 - Telekommunikasiya:
+
+
+# for _ in range(6):
+#     phone_name = input("Enter  phone name: ")
+#     price = float(input("Enter price of the phone: "))
+
+#     formatted_output = f"Phone Name: {phone_name}, Price: ₼{price}"
+#     print(formatted_output)
+
+    
+
+
+
+# Task - 13 - Səyahət Sənayesi:
+
+# airfare_cost = float(input("Enter the cost of the airfare: "))
+# hotel_cost = float(input("Enter the cost of the hotel: "))
+# car_rental_cost = float(input("Enter the cost of car rental: "))
+
+# total_cost = airfare_cost + hotel_cost + car_rental_cost
+
+# print(f"Total Trip Cost: ₼{total_cost}")
+
+
+
+
+
+
+# Task 14 - Tikinti sənayesi:
+
+
