12a57edae693682b8e69ce3516b3eca63156e28b
qoshqarhuseyn <qoshqarhuseyn1@gmail.com>
2024-03-26 11:53:34 +0400

    commit2

---
+++ Task - 7.py
@@ -0,0 +1,158 @@
+
+
+
+# Task - 1
+
+
+# def fun(*a):
+
+#  return (a)
+
+# print(sum(fun(21, 27, 34, 15, 7)))
+
+
+
+
+
+
+
+
+# Task - 2
+
+
+
+
+# def unique_list(l):
+
+#     x = []
+    
+
+#     for a in l:
+   
+#         if a not in x:
+         
+#             x.append(a)
+    
+
+#     return x
+
+
+# print(unique_list([1, 2, 3, 3, 3, 3, 4, 5])) 
+
+
+
+
+
+
+# Task - 3
+
+
+
+# def calculate_operations(num1, num2):
+    
+#     print("Sum:", num1 + num2)
+    
+#     print("Different:", num1 - num2)
+    
+#     print("Product:", num1 * num2)
+    
+#     print("Quotient:", num1 / num2)
+   
+
+# calculate_operations(10, 5)
+
+
+
+
+
+# Task - 4
+
+
+# def calculateTotalSum(*arguments):
+#     totalSum = 0
+#     for number in arguments:
+#         totalSum += number
+#     print(totalSum)
+
+# calculateTotalSum(23, 45, 55, 12, 17)
+
+
+
+
+
+
+# Task - 8
+
+
+
+
+
+
+
+# def guess_number():
+#     target_number = 7 
+#     guess_count = 0
+
+#     while True:
+#         guess = int(input("Enter a Number: "))
+#         guess_count += 1
+
+#         if guess < target_number:
+#             print("Up")
+#         elif guess > target_number:
+#             print("Down")
+#         else:
+#             print(f"You guessed it! It took you {guess_count} guesses.")
+#             break
+
+# guess_number()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
